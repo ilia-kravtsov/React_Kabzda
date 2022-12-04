b3795204d552e6523658b85bea68c396110e56c1
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-04 19:56:52 +0400

    React_Kabzda_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/*
+function expression
+let someFunc = function(){}
+*/
+// function declaration
+// arrow function
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // действия функции
+    // возврат функией jsx разметки то что выглядит как html является jsx разметкой
+    return (
+        <div>
+            This is App component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
 }
 
 export default App;
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>*</div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,81 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App />);
+
 reportWebVitals();
+
+/*
+Основное отличие typescript от javascript в том что typescript следит еще и за соответствием типов данных
+
+let a = 1;
+a = 5; - yes
+a = 'sting' - no - in typescript - yes - in javascript
+
+let a = {name:victor}
+a = {name:artem}
+
+но при этом
+
+let b; // по умолчанию тип any
+the same let b: any;
+b = 13;
+b = 'string';
+b = true; - так можно
+
+но еще можно явно задать тип данных
+
+let c: string;
+
+c = 'aksdjl' - yes
+c = 12 - no
+c = true - no
+
+можно так
+
+let d: boolean = true
+let d = true
+
+указание типа данных массив чисел
+
+let e = Array<number>;
+
+Если хотим чтобы в массиве были и числа и строки
+
+let f: Array<string | number>
+
+f = [1] - yes
+f = ['string'] - yes
+f = [true] - no
+*/
+
+let users = [
+    {
+        name: 'victor',
+        age: 10,
+        address: {
+            city: "Minsk",
+            country: 'Belarus',
+        }
+    },
+    {
+        name: 'artem',
+        age: 11,
+        address: {
+            city: "Moscow",
+            country: 'Russia',
+        }
+    },
+    {
+        name: 'william',
+        age: 12,
+        address: {
+            city: "Melbourne",
+            country: 'Australia',
+        }
+    },
+]
+
+console.log(users[0].address.country) // Belarus
